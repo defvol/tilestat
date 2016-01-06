d55bb9e663e38e0370465c24879c85601e9b6c28
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-01-05 21:42:04 -0800

    Add strict mode for sanity

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var argv    = require('minimist')(process.argv.slice(2)),
     bbox    = JSON.parse(argv.bbox ? argv.bbox : '[-180, -85, 180, 85]'),
     source  = argv._[0] || 'latest.planet.mbtiles',
