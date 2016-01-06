ce9742e1dc49da45f22b381aca369cbac5fb68d4
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-01-05 21:35:51 -0800

    Generate full paths for tilesets

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var argv    = require('minimist')(process.argv.slice(2)),
     bbox    = JSON.parse(argv.bbox ? argv.bbox : '[-180, -85, 180, 85]'),
     source  = argv._[0] || 'latest.planet.mbtiles',
+    path    = require('path'),
     zoom    = argv.zoom ? parseInt(argv.zoom) : 12;
 
 function usage() {
   console.log('Usage: node index.js --<options> <tileset>');
   console.log('node index.js --bbox="[-115, 32, -115, 32]" --zoom=9 tiles');
 }
+
+console.log(path.join(__dirname, source));
 
 if(argv.help) usage();
 
