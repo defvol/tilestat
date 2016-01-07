5eee31800aa1f266fd5cb753586f9b68cc42836c
Rodolfo Wilhelmy <rwilhelmy@gmail.com>
2016-01-06 16:15:21 -0800

    You forgot your semicolons :rage1:

--- test.js
+++ test.js
@@ -4,16 +4,16 @@ var test  = require('tap').test,
     utils = require('./utils');
 
 test('it finds out whether a prop is an OSM feature', function (t) {
-  t.ok(utils.isFeature('highway'), 'highway is a feature')
-  t.ok(!utils.isFeature('_changeset'), '_changeset is not a feature')
-  t.end()
-})
+  t.ok(utils.isFeature('highway'), 'highway is a feature');
+  t.ok(!utils.isFeature('_changeset'), '_changeset is not a feature');
+  t.end();
+});
 
 test('it extracts tags out of properties', function(t) {
   var props = { _changeset: 12859902, highway: 'residential' },
       got   = utils.getTags(props),
       want  = ['highway'];
   t.similar(got, want, 'should get a feature called highway');
-  t.end()
-})
+  t.end();
+});
 
